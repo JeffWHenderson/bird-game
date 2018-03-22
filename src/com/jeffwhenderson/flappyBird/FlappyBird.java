326243c523e569d0fb04ccaca914854431995438
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-22 17:49:54 -0400

    did some initial set up in the constructor of the FlappyBird class.

--- src/com/jeffwhenderson/flappyBird/FlappyBird.java
+++ src/com/jeffwhenderson/flappyBird/FlappyBird.java
@@ -1,5 +1,21 @@
 package com.jeffwhenderson.flappyBird;
 
-public class FlappyBird {
+import javax.swing.JFrame;
 
+public class FlappyBird {
+	public static FlappyBird flappyBird;
+	public final int DEFAULT_WIDTH = 700;
+	public final int DEFAULT_HEIGHT = 700;
+	
+	public FlappyBird() {
+		JFrame jframe = new JFrame();
+		
+		jframe.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		jframe.setVisible(1 > 0);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // this line will close program out properly
+	}
+	
+	public static void main(String[] args) {
+		flappyBird = new FlappyBird();
+	}
 }
