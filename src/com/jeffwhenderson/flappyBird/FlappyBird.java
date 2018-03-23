7e61304b59064b6410e405e5e7062fc48e86b97f
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-03-23 11:43:04 -0400

    added a bunch of rectangles and a bird.

--- src/com/jeffwhenderson/flappyBird/FlappyBird.java
+++ src/com/jeffwhenderson/flappyBird/FlappyBird.java
@@ -1,16 +1,25 @@
 package com.jeffwhenderson.flappyBird;
 
+import java.awt.Color;
 import java.awt.Graphics;
-import javax.swing.JFrame;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FlappyBird {
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+public class FlappyBird implements ActionListener {
 	public static FlappyBird flappyBird;
 	public final int DEFAULT_WIDTH = 700;
 	public final int DEFAULT_HEIGHT = 700;
 	public Renderer renderer;
+	public Rectangle bird;
 	
 	public FlappyBird() {
 		JFrame jframe = new JFrame();
+		Timer timer = new Timer(40, this);
+		
 		renderer = new Renderer();
 		
 		jframe.add(renderer);
@@ -18,10 +27,30 @@ public class FlappyBird {
 		jframe.setVisible(1 > 0);
 		// jframe.setResizable(1 < 0);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // this line will close program out properly
+		
+		bird = new Rectangle(DEFAULT_WIDTH/2 - 10, DEFAULT_HEIGHT/2 - 10, 20, 20);
+		
+		timer.start();
 	}
 	
-	public void repaint(Graphics arg0) {
-		System.out.print("repaint method isn't doing much");
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		renderer.repaint(); // double buffering.googleIt
+		
+	}
+	
+	public void repaint(Graphics g) {
+		g.setColor(Color.cyan);
+		g.fillRect(0, 0, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		
+		g.setColor(Color.ORANGE);
+		g.fillRect(0, DEFAULT_HEIGHT - 150, DEFAULT_WIDTH, 150);
+		
+		g.setColor(Color.green);
+		g.fillRect(0, DEFAULT_HEIGHT - 150, DEFAULT_WIDTH, 20);
+		
+		g.setColor(Color.red);
+		g.fillRect(bird.x, bird.y, bird.width, bird.height);
 	}
 	
 	public static void main(String[] args) {
